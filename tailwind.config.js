218d4639ddeb3ba27294eff8a2cb2b0c5183be01
Tai〽️ <taitran@sandiego.edu>
2025-12-27 21:40:25 -0800

    Added custom screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-   prefix: 'tw-',               // Add a prefix to avoid collisions with existing CSS
+  prefix: "tw-", // Add a prefix to avoid collisions with existing CSS
+  important: true,
   corePlugins: {
-    preflight: false,          // Disable Tailwind’s CSS reset to preserve your legacy styles
+    preflight: false, // Disable Tailwind’s CSS reset to preserve your legacy styles
   },
   content: [
     "./src/**/*.{js,jsx,ts,tsx}", // Scan all React components for Tailwind classes
   ],
   theme: {
-    extend: {},                // Keep default theme; extend if needed
+    screens: {
+      'tw-max-xs': { max: '479px'},
+      'tw-max-sm': { max: '639px'},
+      'tw-max-md': { max: '768px'},
+    }, // Change default to max-width or range-based queries
+    extend: {}, // Keep default theme; extend if needed
   },
-  plugins: [],                 // Add plugins later if needed
-}
-
+  plugins: [], // Add plugins later if needed
+};
